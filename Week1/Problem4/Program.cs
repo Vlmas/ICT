a17cf419831031ffba8c8322af280a73866fe55b
Vlmas <aalmas2001@gmail.com>
2021-01-30 21:46:06 +0600

    'init'

---
+++ Week1/Problem4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Problem4
+{
+    class Program
+    {
+        public static bool ArrayStringsAreEqual(string[] word1, string[] word2)
+        {
+            string str1 = "", str2 = "";
+
+            foreach(var w in word1)
+            {
+                str1 += w;
+            }
+            foreach (var w in word2)
+            {
+                str2 += w;
+            }
+
+            return (string.Equals(str1, str2));
+        }
+        static void Main(string[] args)
+        {
+            string[] word1 = {"ab", "c"};
+            string[] word2 = { "a", "bc" };
+
+            Console.WriteLine(ArrayStringsAreEqual(word1,word2));
+        }
+    }
+}
