8bda406708eb08295e392910c57183c17e21b870
Vlmas <aalmas2001@gmail.com>
2021-03-12 16:52:36 +0600

    'file-manager'

---
+++ FarManager/Manager/Layer.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Manager
+{
+    class Layer
+    {
+        public DirectoryInfo DirInfo { get; set; }
+        public FileInfo FilesInfo { get; set; }
+        public int Position { get; set; }
+        public List<FileSystemInfo> Content { get; set; }
+        public Layer() { }
+
+        public Layer(DirectoryInfo DirInfo, int Position)
+        {
+            this.DirInfo = DirInfo;
+            this.Position = Position;
+            this.Content = new List<FileSystemInfo>();
+            this.Content.AddRange(this.DirInfo.GetDirectories());
+            this.Content.AddRange(this.DirInfo.GetFiles());
+        }
+
+        public Layer(FileInfo FilesInfo)
+        {
+            this.FilesInfo = FilesInfo;
+        }
+
+        public void PrintInfo()
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            int count = 0;
+
+            foreach(DirectoryInfo di in DirInfo.GetDirectories()) 
+            {
+                if(count == Position)
+                {
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.Title = CalculateSize(di.FullName).ToString();
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.White;
+                }
+
+                Console.WriteLine(di.Name);
+                count++;
+            }
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+            foreach(FileInfo fi in DirInfo.GetFiles())
+            {
+                if(count == Position)
+                {
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.Title = CalculateSize(fi.FullName).ToString();
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.White;
+                }
+
+                Console.WriteLine(fi.Name);
+                count++;
+            }
+        }
+
+        public void PrintContent()
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            string[] lines = File.ReadAllLines(FilesInfo.FullName);
+
+            foreach(string line in lines) 
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        public FileSystemInfo GetCurrentObject()
+        {
+            return this.Content[Position];
+        }
+
+        public void SetNewPosition(int inc)
+        {
+            if(inc > 0)
+            {
+                Position++;
+            }
+            else
+            {
+                Position--;
+            }
+
+            if(Position >= Content.Count)
+            {
+                Position = 0;
+            }
+            else if(Position < 0)
+            {
+                Position = Content.Count - 1;
+            }
+        }
+
+        static double CalculateSize(string folder)
+        {
+            double size = 0;
+            
+            if (!Directory.Exists(folder))
+            {
+                return -1;
+            }
+            else
+            {
+                try
+                {
+                    foreach (string file in Directory.GetFiles(folder))
+                    {
+                        if (File.Exists(file))
+                        {
+                            FileInfo fi = new FileInfo(file);
+                            size += fi.Length;
+                        }
+                    }
+                }
+                catch (NotSupportedException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            return size;
+        }
+    }
+}
---
+++ FarManager/Manager/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Manager
+{
+    class Program
+    {
+        const string PATH = @"C:\Programming";
+        static void Manage(string path)
+        {
+            ConsoleKeyInfo keyInfo;
+            bool exit = false;
+            bool shareMode = false;
+
+            Stack<Layer> history = new Stack<Layer>();
+            history.Push(new Layer(new DirectoryInfo(path), 0));
+
+            while(!exit)
+            {
+                if(!shareMode) 
+                {
+                    history.Peek().PrintInfo();
+                }
+                else
+                {
+                    history.Peek().PrintContent();
+                }
+                keyInfo = Console.ReadKey(true);
+
+                switch(keyInfo.Key)
+                {
+                    case ConsoleKey.Enter:
+                        if (history.Peek().GetCurrentObject().GetType() == typeof(DirectoryInfo))
+                        {
+                            history.Push(new Layer(history.Peek().GetCurrentObject() as DirectoryInfo, 0));
+                        }
+                        else if(history.Peek().GetCurrentObject().GetType() == typeof(FileInfo))
+                        {
+                            history.Push(new Layer(history.Peek().GetCurrentObject() as FileInfo));
+                            shareMode = true;
+                        }
+                        break;
+                    case ConsoleKey.W:
+                        history.Peek().SetNewPosition(-1);
+                        break;
+                    case ConsoleKey.S:
+                        history.Peek().SetNewPosition(1);
+                        break;
+                    case ConsoleKey.Escape:
+                        history.Pop();
+                        shareMode = false;
+                        break;
+                    case ConsoleKey.N:
+                        CreateDirectory();
+                        break;
+                    case ConsoleKey.M:
+                        DeleteDirectory(Console.ReadLine());
+                        break;
+                    case ConsoleKey.E:
+                        exit = true;
+                        break;
+                }
+            }
+        }
+
+        public static void CreateDirectory()
+        {
+            Random random = new Random();
+            string path = PATH + @"\" + random.Next(0, 100);
+
+            if(!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+
+        public static void DeleteDirectory(string name)
+        {
+            string path = PATH + @"\" + name;
+
+            if(Directory.Exists(path)) 
+            {
+                foreach (string f in Directory.GetFiles(path))
+                {
+                    File.Delete(f);
+                }
+                foreach (string f in Directory.GetDirectories(path))
+                {
+                    DeleteDirectory(f);
+                }
+                Directory.Delete(path);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Manage(PATH);
+        }
+    }
+}
