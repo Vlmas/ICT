ac8a9695efa8d54243e4d1bfabece529a6848f54
Vlmas <aalmas2001@gmail.com>
2021-04-28 22:44:06 +0600

    'problems'

---
+++ Week1Tasks/Problem3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Problem3
+{
+    class Program
+    {
+        public static int NumberOfSteps(int num)
+        {
+            int cnt = 0;
+
+            while(num != 0)
+            {
+                if(num%2==0)
+                {
+                    num /= 2;
+                }
+                else
+                {
+                    num -= 1;
+                }
+                cnt++;
+            }
+
+            return cnt;
+        }
+
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(NumberOfSteps(num));
+        }
+    }
+}
---
+++ Week1Tasks/TestingPrinciples/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TestingPrinciples
+{
+    class Human
+    {
+        private string fullName;
+        private string university;
+        public Human(string fullName,string university) 
+        {
+            this.fullName = fullName;
+            this.university = university;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.fullName} studies at {this.university}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, university;
+            string[] line = Console.ReadLine().Split(' ');
+            name = line[0];
+            university = line[1];
+
+            Human human = new Human(name,university);
+
+            Console.WriteLine(human);
+        }
+    }
+}
